903df8ca319a522368f919066b29047b33edd673
Connor Dieck <connordieck@gmail.com>
2021-06-03 20:41:12 -0400

    finished wordfinder.py and modified words.txt to test code

--- wordfinder.py
+++ wordfinder.py
@@ -7,23 +7,74 @@ class WordFinder:
     "Finds random words from a dictionary"
 
     def __init__(self, file):
-        self.file = file
-        self.wordlist = []
+        self.word_file = open(file)
+        
+        self.words = self.readwords()
 
-        self.readwords()
+        print(f"{len(self.words)} words read")
     
     def readwords(self):
-        file = open(self.file, "r")
+        "Reads words from file and prints the numbers of lines read"
         numwords = 0
 
-        for line in file:
-            self.wordlist.append(line)
-            numwords += 1
+        return [word.strip() for word in self.word_file]
         
-        file.close()
-        print(f"{numwords} words read")
     
     def random(self):
-        randword = choice(self.wordlist)
-        randword = randword[0:(len(randword)-1)]
+        "Returns a random word from file"
+        randword = choice(self.words)
         return randword
+
+class SpecialWordFinder(WordFinder):
+    def __init__(self, file):
+        super().__init__(file)
+
+    def readwords(self):
+        "Reads words from file and prints the numbers of lines read"
+        return [word.strip() for word in self.word_file if word.strip() and not word.startswith("#")]
+
+# Original attempt:
+# -------------------------
+# class WordFinder:
+#     "Finds random words from a dictionary"
+
+#     def __init__(self, file):
+#         self.file = file
+#         self.wordlist = []
+
+#         self.readwords()
+    
+#     def readwords(self):
+#         "Reads words from file and prints the numbers of lines read"
+#         file = open(self.file, "r")
+#         numwords = 0
+
+#         for line in file:
+#             self.wordlist.append(line)
+#             numwords += 1
+        
+#         file.close()
+#         print(f"{numwords} words read")
+    
+#     def random(self):
+#         "Returns a random word from file"
+#         randword = choice(self.wordlist)
+#         randword = randword[0:(len(randword)-1)]
+#         return randword
+
+# class SpecialWordFinder(WordFinder):
+#     def __init__(self, file):
+#         super().__init__(file)
+
+#     def readwords(self):
+#         "Reads words from file and prints the numbers of lines read"
+#         file = open(self.file, "r")
+#         numwords = 0
+
+#         for line in file:
+#             if line != "" or "#" not in line:
+#                 self.wordlist.append(line)
+#                 numwords += 1
+        
+#         file.close()
+#         print(f"{numwords} words read")
