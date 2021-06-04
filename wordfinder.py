bd04fef1b81e2e61ba5d3a6f27a6837752858a13
Connor Dieck <connordieck@gmail.com>
2021-06-03 20:03:53 -0400

    finished serial.py and initial code for wordfinder.py

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,29 @@
+from random import choice
+
 """Word Finder: finds random words from a dictionary."""
 
 
 class WordFinder:
-    ...
+    "Finds random words from a dictionary"
+
+    def __init__(self, file):
+        self.file = file
+        self.wordlist = []
+
+        self.readwords()
+    
+    def readwords(self):
+        file = open(self.file, "r")
+        numwords = 0
+
+        for line in file:
+            self.wordlist.append(line)
+            numwords += 1
+        
+        file.close()
+        print(f"{numwords} words read")
+    
+    def random(self):
+        randword = choice(self.wordlist)
+        randword = randword[0:(len(randword)-1)]
+        return randword
